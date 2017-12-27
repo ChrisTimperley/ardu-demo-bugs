1f1d8b3ae6c00ba42eaf7e951ddf81abf8db96e6
ChrisTimperley <christimperley@googlemail.com>
2017-12-27 16:35:42 +0800

    implemented 'issue_mission'

--- test.py
+++ test.py
@@ -4,8 +4,9 @@
 # issue commands/missions to the rover.
 #
 import dronekit
+from typing import List
 from dronekit_sitl import SITL
-from dronekit import Command, CommandSequence
+from dronekit import Vehicle, Command, CommandSequence
 
 
 def parse_command(s: str) -> Command:
@@ -17,37 +18,49 @@ def parse_command(s: str) -> Command:
     return Command(0, 0, *args)
 
 
-def load_mission(fn: str) -> CommandSequence:
+def load_mission(fn: str) -> List[Command]:
     """
-    Loads a mission from a given file and converts it into a
-    CommandSequence object.
+    Loads a mission from a given file and converts it into a list of
+    Command objects.
     """
     cmds = []
     with open(fn, 'r') as f:
         for line in f[1:]:
             cmd = parse_command(line)
             cmds.append(cmd)
-    return CommandSequence(cmds)
+    return cmds
 
 
-try:
-    sitl = SITL(binary)
-    vehicle = connect(sitl.connection_string(), wait_ready=True)
-
-    # download the mission
-    # (note: 'wait_ready' blocks until mission has finished downloading)
-    cmds = BLAH
-    cmds.download()
-    cmds.wait_ready()
-
-    # trigger by the mission by switching the vehicle's mode to "AUTO"
-    vehicle.mode = dronekit.Mode("AUTO")
-
-    # monitor the mission
-    # could use "add_attribute_listener" to listen to next_waypoint attribute?
+def issue_mission(vehicle: Vehicle, commands: List[Command]) -> None:
+    """
+    Issues (but does not trigger) a mission, provided as a list of commands,
+    to a given vehicle.
+    Blocks until the mission has been downloaded onto the vehicle.
+    """
+    vcmds = vehicle.commands
+    vcmds.clear()
+    for command in commands:
+        vcmds.add(command)
+    vcmds.download()
+    vcmds.wait_ready()
 
 
-finally:
-    if vehicle:
-        vehicle.close()
-    sitl.stop()
+def execute_mission(fn: str) -> bool:
+    mission = load_mission(fn)
+
+    try:
+        sitl = SITL(binary)
+        vehicle = connect(sitl.connection_string(), wait_ready=True)
+        issue_mission(vehicle, mission)
+
+        # trigger the mission by switching the vehicle's mode to "AUTO"
+        vehicle.mode = dronekit.Mode("AUTO")
+
+        # monitor the mission
+        # could use "add_attribute_listener" to listen to next_waypoint attribute?
+
+
+    finally:
+        if vehicle:
+            vehicle.close()
+        sitl.stop()
