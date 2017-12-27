fcac0722c8a24504bf1e2b8d852d42e922d0cc6e
ChrisTimperley <christimperley@googlemail.com>
2017-12-27 16:29:29 +0800

    implemented 'parse_command'

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ def parse_command(s: str) -> Command:
     Parses a line from a mission file into its corresponding Command
     object in Dronekit.
     """
-    raise NotImplementedError
+    args = [float(x) for x in s.split()]
+    return Command(0, 0, *args)
 
 
 def load_mission(fn: str) -> CommandSequence:
@@ -28,14 +29,22 @@ def load_mission(fn: str) -> CommandSequence:
             cmds.append(cmd)
     return CommandSequence(cmds)
 
+
 try:
     sitl = SITL(binary)
     vehicle = connect(sitl.connection_string(), wait_ready=True)
+
+    # download the mission
+    # (note: 'wait_ready' blocks until mission has finished downloading)
+    cmds = BLAH
+    cmds.download()
+    cmds.wait_ready()
 
     # trigger by the mission by switching the vehicle's mode to "AUTO"
     vehicle.mode = dronekit.Mode("AUTO")
 
     # monitor the mission
+    # could use "add_attribute_listener" to listen to next_waypoint attribute?
 
 
 finally:
