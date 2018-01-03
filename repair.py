b8bc9484848d66187fa1809d87d4c67181110b49
ChrisTimperley <christimperley@googlemail.com>
2018-01-04 01:07:00 +0800

    another fix to repair script

--- repair.py
+++ repair.py
@@ -5,6 +5,9 @@ from darjeeling.repair import repair
 
 
 if __name__ == '__main__':
+    # how many threads would you like to use?
+    threads = 32
+
     # construct a description of the problem for Darjeeling
     in_files = ['APMrover2/commands_logic.cpp']
 
@@ -16,7 +19,7 @@ if __name__ == '__main__':
 
     # Since we can't obtain coverage information for ArduPilot right now,
     # this isn't actually used.
-    # metric = bugzoo.localization.suspiciousness.tarantula
+    metric = bugzoo.localization.suspiciousness.tarantula
 
     # this will run indefinitely until a repair is found.
-    repair(problem, metric)
+    repair(problem, metric, threads=threads)
