82085870fb697e4b80010c132eb3f801f9ac637b
ChrisTimperley <christimperley@googlemail.com>
2018-03-13 16:56:00 -0400

    refactoring test harness

--- test.py
+++ test.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python
+#
+# This file is responsible for implementing a GenProg-style test harness for
+# a given scenario.
+#
 from __future__ import print_function
-from sys import exit
+from pprint import pprint as pp
 import math
 import sys
-from pprint import pprint as pp
-from helper import distance
-from mission_runner import load_mission, execute_mission
+
 from dronekit import LocationGlobal
+
+import mission
+import helper
 
 
 class TestCase(object):
-    # TODO: add time limit
     def __init__(self, filename, time_limit, end_pos):
+        """
+        Constructs a new test case.
+
+        Parameters:
+            filename: the location of the WPL file (i.e., mission file) for
+                this test case.
+            time_limit: the number of seconds that this test is allowed to
+                run without completing before it is considered to have failed.
+            end_pos: the expected position of the vehicle following the
+                completion of the test.
+        """
         assert isinstance(end_pos, LocationGlobal)
         self.__filename = filename
         self.__end_pos = end_pos
         self.__time_limit = time_limit
-        self.__mission = load_mission(filename)
+        self.__mission = mission.Mission.from_file(filename)
 
     @property
     def time_limit(self):
+        """
+        The number of seconds that this test case is allowed to run without
+        completing before it is considered to be a failure.
+        """
         return self.__time_limit
-
-    @property
-    def filename(self):
-        return self.__filename
 
     @property
     def mission(self):
@@ -32,6 +47,9 @@ class TestCase(object):
 
     @property
     def end_pos(self):
+        """
+        The intended location of the vehicle after completing the mission.
+        """
         return self.__end_pos
 
     def execute(self):
@@ -45,7 +63,7 @@ class TestCase(object):
             return (False, "failed to reach last waypoint within time limit")
 
         pos = LocationGlobal(snapshot['lat'], snapshot['lon'], snapshot['alt'])
-        dist = distance(self.end_pos, pos)
+        dist = helper.distance(self.end_pos, pos)
         if dist < 2.0:
             return (True, None)
         else:
@@ -55,21 +73,21 @@ class TestCase(object):
 if __name__ == '__main__':
     # construct the test suite
     tests = {
-        'p1': TestCase('missions/rover-not-broke.txt', 60,
-                       LocationGlobal(40.0713758, -105.2297839, 1583.67)),
-        'n1': TestCase('missions/rover-broke.txt', 60,
-                       LocationGlobal(40.0713758, -105.2297839, 1583.67))
+        'p1': TestCase('missions/scenario.wpl', 60,
+                       end_pos=LocationGlobal(40.0713758, -105.2297839, 1583.67)),
+        'n1': TestCase('missions/scenario.wpl', 60,
+                       end_pos=LocationGlobal(40.0713758, -105.2297839, 1583.67))
     }
 
     # which test does the user wish to execute?
     if len(sys.argv) != 2:
         print("USAGE: ./test.py [test-id]")
-        exit(2)
+        sys.exit(2)
 
     test_id = sys.argv[1]
     if test_id not in tests:
         print("Unrecognised test identifier provided.")
-        exit(2)
+        sys.exit(2)
 
     # execute the specified test
     test = tests[test_id]
@@ -78,4 +96,4 @@ if __name__ == '__main__':
         print("PASSED")
     else:
         print("FAILED: {}".format(msg))
-        exit(1)
+        sys.exit(1)
