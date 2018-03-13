e17e77d48ab8dfd00dddc5eedb78f1d14e8bccef
ChrisTimperley <christimperley@googlemail.com>
2018-03-13 14:37:22 -0400

    removed unnecessary files

--- auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# mavproxy.py --map
-docker build -t overflow .
-docker run  --rm \
-            -e DISPLAY=$DISPLAY \
-            -v /tmp/.X11-unix:/tmp/.X11-unix \
-            -it overflow \
-            /bin/bash
-            # python test.py
