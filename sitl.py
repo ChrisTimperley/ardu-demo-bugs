ee02ae9f3e4b94f37bbfb4f2e6270a17b5ed9f36
ChrisTimperley <christimperley@googlemail.com>
2018-03-13 20:16:35 -0400

    added from_cfg to SITL

--- sitl.py
+++ sitl.py
@@ -6,46 +6,72 @@ import dronekit_sitl
 
 class SITL(object):
     def from_cfg(cfg):
-        raise NotImplementedError
-        return SITL(model=model,
-                    binary=binary)
+        # TODO which other vehicles do we need to support?
+        vehicle = cfg.get("Mission", "vehicle")
+        if vehicle == 'APMRover2':
+            binary = 'ardurover'
+            model = 'rover'
+        elif vehicle == 'ArduCopter':
+            binary = 'arducopter'
+            model = 'copter'
 
-    def __init__(model='rover', binary='ardurover'):
+        home_lat = cfg.getfloat("Mission", "latitude")
+        home_lon = cfg.getfloat("Mission", "longitude")
+        home_alt = cfg.getfloat("Mission", "altitude")
+        home_heading = cfg.getfloat("Mission", "heading")
+
+        return SITL(vehicle=vehicle,
+                    model=model,
+                    binary=binary,
+                    home_lat=home_lat,
+                    home_lon=home_lon,
+                    home_heading=home_heading)
+
+    def __init__(vehicle,
+                 model,
+                 binary,
+                 home_lat,
+                 home_lon,
+                 home_alt,
+                 home_heading):
         self.__dir_base = '/experiment/source'
-        self.__name_binary = binary
+        self.__vehicle = vehicle
         self.__model = model
         self.__simulator = None
+        self.__home_loc = (home_lat, home_lon, home_alt, home_heading)
+        self.__binary = os.path.join(self.__dir_base, 'build/sitl/bin', binary)
 
     def start(self):
-        binary = os.path.join(self.__dir_base,
-                              'build/sitl/bin',
-                              self.__name_binary)
-
-        # TODO this needs to work
-        # launch mavproxy -- ports aren't being forwarded!
-        # sim_uri = sitl.connection_string()
-        # mavproxy_cmd = \
-        #     "mavproxy.py --console --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501 --out localhost:14550 & /bin/bash"
-        # subprocess.call(mavproxy_cmd, shell=True)
-        # time.sleep(1)
-
-        # TODO allow home to be specified
-        home = [40.071374969556928, -105.22978898137808, 1583.702759, 246]
-
-        # TODO parameters!
-        args = [
-            '--model={}'.format(self.__model),
-            '--home={}'.format(','.join(map(str, home))),
-            '--defaults=/experiment/source/Tools/autotest/default_params/rover.parm'
+        script_sim = os.path.join(self.__dir_base, 'Tools/autotest/sim_vehicle.py')
+        cmd = [
+            script_sim,
+            "-l",
+            "{},{},{},{}".format(*self.__home_loc),
+            "-v",
+            self.__vehicle,
+            "-w",
+            "--no-rebuild "
+            "--ardu-dir ",
+            self.__dir_base,
+            "--ardu-binary",
+            self.__binary,
+            "--mavproxy-args=--state-basedir={}".format(
+                os.path.join(self.experiment_base, "run{}".format(
+                    self.run_count)))
         ]
 
-        # TODO we need to use mavproxy
-        sitl = dronekit_SITL.SITL(binary)
-        sitl.launch([home_arg, model_arg, defaults_arg],
-                    verbose=True,
-                    await_ready=True,
-                    restart=False,
-                    speedup=speedup)
+        # args_dronekit = [
+        #     '--model={}'.format(self.__model),
+        #     '--home={}'.format(','.join(map(str, self.__home_loc))),
+        #     '--defaults=/experiment/source/Tools/autotest/default_params/rover.parm' # TODO fix!
+        # ]
+        #
+        # sitl = dronekit_SITL.SITL(binary)
+        # sitl.launch(args_dronekit,
+        #             verbose=True,
+        #             await_ready=True,
+        #             restart=False,
+        #             speedup=speedup)
 
     def stop(self):
         self.__simulator.close()
